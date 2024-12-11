24513e7f93f072762caf6a8ab43b46ecd5f2aaba
Ariel Yordan <166857353+arielyrd@users.noreply.github.com>
2024-12-11 20:51:08 +0700

    Create database.js
    make database.js

---
+++ cloud computing/API-backend/database.js
@@ -0,0 +1,15 @@
+const admin = require("firebase-admin");
+
+const { initializeApp, cert } = require("firebase-admin/app");
+
+const { getFirestore } = require("firebase-admin/firestore");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+initializeApp({
+  credential: cert(serviceAccount),
+});
+
+const db = getFirestore();
+
+module.exports = { db, admin };
