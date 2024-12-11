c9df4e3caaabbf887547bd012d80c082fd767e1c
Ariel Yordan <166857353+arielyrd@users.noreply.github.com>
2024-12-11 20:37:17 +0700

    Update userAuth.js
    final userAuth.js

--- cloud computing/API-backend/userLogin/userAuth.js
+++ cloud computing/API-backend/userLogin/userAuth.js
@@ -1,1 +1,18 @@
+const express = require("express");
+const {
+  signUp,
+  login,
+  logout,
+  getUserInfo,
+  verifyToken,
+} = require("./userController");
+
+const router = express.Router();
+
+router.post("/signup", signUp);
+router.post("/login", login);
+router.post("/logout", verifyToken, logout);
+router.get("/profile", verifyToken, getUserInfo);
+
+module.exports = router;
 
