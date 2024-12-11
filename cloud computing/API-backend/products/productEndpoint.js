b9ecf9a2e13646427d828cae4a43f20812e4caab
Ariel Yordan <166857353+arielyrd@users.noreply.github.com>
2024-12-11 20:47:45 +0700

    Update productEndpoint.js
    final productEndpoint.js

--- cloud computing/API-backend/products/productEndpoint.js
+++ cloud computing/API-backend/products/productEndpoint.js
@@ -1,1 +1,13 @@
+const express = require("express");
+const { getProductById, createProduct } = require("./productHandler");
+
+const router = express.Router();
+
+// GET: Ambil produk berdasarkan ID
+router.get("/:product_id", getProductById);
+
+// POST: Tambah produk baru
+router.post("/", createProduct);
+
+module.exports = router;
 
