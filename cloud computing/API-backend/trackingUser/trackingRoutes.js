fbf6a2943e9a801db6a4f87d7e5b120c3ed96db4
Ariel Yordan <166857353+arielyrd@users.noreply.github.com>
2024-12-11 20:52:51 +0700

    Create trackingRoutes.js

---
+++ cloud computing/API-backend/trackingUser/trackingRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const {
+  addAllergen,
+  editAllergen,
+  getAllAllergens,
+  deleteAllergen,
+} = require("./trackingController");
+
+
+const router = express.Router();
+
+// Endpoint untuk menambahkan allergen
+router.post("/allergens", addAllergen);
+
+// Endpoint untuk mendapatkan semua allergen (dengan opsi filter nama)
+router.get("/allergens", getAllAllergens);
+
+// Endpoint untuk mengubah allergen berdasarkan ID
+router.put("/allergens/:id", editAllergen);
+
+// Endpoint untuk menghapus allergen berdasarkan ID
+router.delete("/allergens/:id", deleteAllergen);
+
+module.exports = router;
