89376d22629bce580a6a8effaef023621e751fc4
Ariel Yordan <166857353+arielyrd@users.noreply.github.com>
2024-12-11 20:36:36 +0700

    Update server.js
    final server.js

--- cloud computing/API-backend/server.js
+++ cloud computing/API-backend/server.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+const cors = require("cors");
+const userAuthRoutes = require("./userLogin/userAuth");
+const userTrackingRoutes = require("./trackingUser/trackingRoutes");
+const productRoutes = require("./products/productEndpoint");
+const scanningRoutes = require("./scanningBarcode/scanningEndpoint");
 
 const app = express();
-const port = 8000;
+const port = 8080;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use(routes);
+app.use("/userLogin", userAuthRoutes);
+app.use("/trackingUser", userTrackingRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/scanning", scanningRoutes);
 
-// Start the server
+app.get("/", (req, res) => {
+  res.send("Response success!");
+});
+
 app.listen(port, () => {
   console.log(`Server berjalan pada http://localhost:${port}`);
 });
