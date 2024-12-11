cfcafd4dd54e60674b8aa34f87d3b6b7a194ba1a
Ariel Yordan <166857353+arielyrd@users.noreply.github.com>
2024-12-11 20:50:11 +0700

    Update scanningEndpoint.js
    final scanningEndpoint.js

--- cloud computing/API-backend/scanningBarcode/scanningEndpoint.js
+++ cloud computing/API-backend/scanningBarcode/scanningEndpoint.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const { barcodeScan, getScanHistory } = require("./scanningController");
 
+const router = express.Router();
+
+router.post("/barcodeScan", barcodeScan);
+router.get("/history", getScanHistory);
+
+module.exports = router;
